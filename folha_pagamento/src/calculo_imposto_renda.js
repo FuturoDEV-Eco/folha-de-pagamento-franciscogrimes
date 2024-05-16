fc37024daa9d0b9c582aa7630668059c12600f72
franciscogrimes <fgsieg1831@gmail.com>
2024-05-16 20:28:01 -0300

    Inserindo o input para adicionar o salário Bruto

--- folha_pagamento/src/calculo_imposto_renda.js
+++ folha_pagamento/src/calculo_imposto_renda.js
@@ -6,39 +6,48 @@
 // De R$ 3.751,06 até R$ 4.664,68	22,5%	R$ 662,77
 // Acima de R$ 4.664,69	27,5%	R$ 896,00
 
-let valor = 2500.65;
+const readline = require("readline");
 
-if (Number(valor) >= 2259.21 && Number(valor) <= 2826.55) {
-  parcelaIR = valor * 0.075;
+const input = readline.createInterface(process.stdin, process.stdout);
 
-  console.log(
-    "O desconto do seu IR caiu no cálulo da alíquota de 7,5%\n" +
-      `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-        2
-      )}`
-  );
-} else if (Number(valor) >= 2826.66 && Number(valor) <= 3751.05) {
-  parcelaIR = valor * 0.15;
-  console.log(
-    "O desconto do seu IR caiu no cálulo da alíquota de 15%\n" +
-      `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-        2
-      )}`
-  );
-} else if (Number(valor) >= 3751.06 && Number(valor) <= 4664.68) {
-  parcelaIR = valor * 0.225;
-  console.log(
-    "O desconto do seu IR caiu no cálulo da alíquota de 22,5%\n" +
-      `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-        2
-      )}`
-  );
-} else if (Number(valor) >= 4664.69) {
-  parcelaIR = valor * 0.275;
-  console.log(
-    `O desconto do seu IR caiu no cálulo da alíquota de 27,5%` /
-      n`O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-        2
-      )}`
-  );
-}
+input.question("Insira o valor do seu salario bruto:", (value) => {
+  const salarioBruto = Number(value);
+
+  if (salarioBruto >= 2259.21 && salarioBruto <= 2826.55) {
+    const parcelaIR = salarioBruto * 0.075;
+    console.log(
+      "O desconto do seu IR caiu no cálculo da alíquota de 7,5%\n" +
+        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
+          2
+        )}`
+    );
+  } else if (salarioBruto >= 2826.66 && salarioBruto <= 3751.05) {
+    const parcelaIR = salarioBruto * 0.15;
+    console.log(
+      "O desconto do seu IR caiu no cálculo da alíquota de 15%\n" +
+        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
+          2
+        )}`
+    );
+  } else if (salarioBruto >= 3751.06 && salarioBruto <= 4664.68) {
+    const parcelaIR = salarioBruto * 0.225;
+    console.log(
+      "O desconto do seu IR caiu no cálculo da alíquota de 22,5%\n" +
+        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
+          2
+        )}`
+    );
+  } else if (salarioBruto >= 4664.69) {
+    const parcelaIR = salarioBruto * 0.275;
+    console.log(
+      "O desconto do seu IR caiu no cálculo da alíquota de 27,5%\n" +
+        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
+          2
+        )}`
+    );
+  } else {
+    console.log("Salário bruto fora da faixa de cálculo de IR.");
+  }
+
+  input.close();
+});
