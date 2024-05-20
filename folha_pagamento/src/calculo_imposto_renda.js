7399a23941de6de62cb6fff2dae257f71862b07a
franciscogrimes <fgsieg1831@gmail.com>
2024-05-20 20:04:28 -0300

    implementação da função do salario liquido

--- folha_pagamento/src/calculo_imposto_renda.js
+++ folha_pagamento/src/calculo_imposto_renda.js
@@ -6,48 +6,18 @@
 // De R$ 3.751,06 até R$ 4.664,68	22,5%	R$ 662,77
 // Acima de R$ 4.664,69	27,5%	R$ 896,00
 
-const readline = require("readline");
-
-const input = readline.createInterface(process.stdin, process.stdout);
-
-input.question("Insira o valor do seu salario bruto:", (value) => {
-  const salarioBruto = Number(value);
-
+function calculaIR(salarioBruto) {
+  let parcelaIR = 0;
   if (salarioBruto >= 2259.21 && salarioBruto <= 2826.55) {
-    const parcelaIR = salarioBruto * 0.075;
-    console.log(
-      "O desconto do seu IR caiu no cálculo da alíquota de 7,5%\n" +
-        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-          2
-        )}`
-    );
+    parcelaIR = salarioBruto * 0.075;
   } else if (salarioBruto >= 2826.66 && salarioBruto <= 3751.05) {
-    const parcelaIR = salarioBruto * 0.15;
-    console.log(
-      "O desconto do seu IR caiu no cálculo da alíquota de 15%\n" +
-        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-          2
-        )}`
-    );
+    parcelaIR = salarioBruto * 0.15;
   } else if (salarioBruto >= 3751.06 && salarioBruto <= 4664.68) {
-    const parcelaIR = salarioBruto * 0.225;
-    console.log(
-      "O desconto do seu IR caiu no cálculo da alíquota de 22,5%\n" +
-        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-          2
-        )}`
-    );
+    parcelaIR = salarioBruto * 0.225;
   } else if (salarioBruto >= 4664.69) {
-    const parcelaIR = salarioBruto * 0.275;
-    console.log(
-      "O desconto do seu IR caiu no cálculo da alíquota de 27,5%\n" +
-        `O valor da parcela a ser descontada pelo seu IR é R$ ${parcelaIR.toFixed(
-          2
-        )}`
-    );
-  } else {
-    console.log("Salário bruto fora da faixa de cálculo de IR.");
+    parcelaIR = salarioBruto * 0.275;
   }
+  return parcelaIR.toFixed(2);
+}
 
-  input.close();
-});
+module.exports = calculaIR;
