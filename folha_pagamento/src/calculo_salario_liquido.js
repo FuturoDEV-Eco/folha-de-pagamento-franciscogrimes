187cf1aa9f06d8eb113e4a5ae83d73a6b3805a45
franciscogrimes <fgsieg1831@gmail.com>
2024-05-20 22:03:06 -0300

    Finalização do exercicio 5

--- folha_pagamento/src/calculo_salario_liquido.js
+++ folha_pagamento/src/calculo_salario_liquido.js
@@ -1,21 +1,10 @@
-const readline = require("readline");
-const input = readline.createInterface(process.stdin, process.stdout);
-
 const calculaIR = require("./calculo_imposto_renda");
 const calculaINSS = require("./calculo_inss");
 
-input.question("Qual o valor do salário bruto?", (value) => {
-  const salarioBruto = parseFloat(value);
+function calculaSalarioLiquido(salarioBruto) {
+  const IR = parseFloat(calculaIR(salarioBruto));
+  const INSS = parseFloat(calculaINSS(salarioBruto));
+  return (salarioBruto - IR - INSS).toFixed(2);
+}
 
-  function salarioLiquido(salarioBruto) {
-    const IR = calculaIR(salarioBruto);
-    const INSS = calculaINSS(salarioBruto);
-    const resultado = salarioBruto - IR - INSS;
-
-    console.log(`O valor do salário líquido é R$ ${resultado.toFixed(2)}`);
-  }
-
-  salarioLiquido(salarioBruto);
-
-  input.close();
-});
+module.exports = calculaSalarioLiquido;
